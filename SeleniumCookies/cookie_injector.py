4ff8513453ec3ed47f08835ed0c609764245fab7
D-E-F-E-A-T <kushchoudhary8@gmail.com>
2020-01-22 04:38:45 +0530

    added status code to cokkie_injector

--- SeleniumCookies/cookie_injector.py
+++ SeleniumCookies/cookie_injector.py
@@ -17,9 +17,33 @@ try:
 except:
     pass
 
+LoadingFailure = 5001
+FirefoxCookieNotFound = 4001
+ChromeCookieNotFound = 4002
+JsonParsingError = 5002
+LZ4ParsingError = 5003
+CreateCookie = 2001
+LoadCookie = 2002
+LoadFirefoxCookie = 2003
+LoadChromeCookie = 2004
+
+
+CodeMap = {
+    5001: 'Error in Cookie Loading',
+    5002: 'Error parsing firefox session JSON',
+    5003: 'Error parsing firefox session JSON LZ4',
+    4001: 'Failed to find Firefox Cookie',
+    4002: 'Failed to find Chrome cookie',
+    2001: 'Creating Cookie',
+    2002: 'Loading Cookie from CookieJar',
+    2003: 'Loading cookie from Firefox',
+    2004: 'Loading cookie from Chrome'
+
+}
+
 
 LEVEL = logging.INFO
-FORMAT = "{'FileName': 'cookie_injector' ,'DateTime':%(asctime)s, 'LevelName':%(levelname)s, 'Message':%(message)s}"
+FORMAT = "{'FileName': 'cookie_injector' ,'DateTime':'%(asctime)s', 'LevelName':'%(levelname)s', 'Message':%(message)s}"
 logging.basicConfig(filename='SeleniumCookies/selenium_cookies.log',
                     filemode='a', level=LEVEL, format=FORMAT, datefmt='%d-%b-%y %H:%M:%S')
 
@@ -27,14 +51,16 @@ cookies = []
 
 
 def inject_cookie():
-    logging.info('Starting to take Cookies')
+    logging.info("{'StatusCode':'%s' ,'Status':'%s'}",
+                 LoadCookie, CodeMap[LoadCookie])
     _cookiejar = wrapper.load()
     for webname in _cookiejar._cookies:
         for endpoint in _cookiejar._cookies[webname]:
             for processed_cookie in _cookiejar._cookies[webname][endpoint]:
                 cookies.append(
                     _cookiejar._cookies[webname][endpoint][processed_cookie].__dict__)
-    logging.info(f'{len(cookies)} cookies loaded')
+    logging.info(
+        "{'StatusCode':'%s', 'Status':'%s cookies loaded'}", 2000, len(cookies))
     return cookies
 
 
